ced4d1dfc380e1131247ab1f630a1db33808b931
Rizky Khoiruddin <rizky.khoiruddin17@gmail.com>
2023-12-02 21:43:29 +0700

    02/12/2023

--- app/components/Jumbotron.js
+++ app/components/Jumbotron.js
@@ -8,7 +8,7 @@ function Header() {
             <div className='flex flex-col basis-[50%] items-start justify-center lg:ms-10 ms-0 me-0 gap-5'>
                 <h3 className='text-2xl font-semibold uppercase tracking-wide'>Rizky Khoiruddin</h3>
                 <h1 className='text-blue text-5xl font-bold'>Fullstack Developer</h1>
-                <p className='lg:text-lg text-base leading-7'>
+                <p className='lg:text-lg text-base lg:leading-8 leading-7'>
                     Saya menerapkan desain antarmuka pengguna dan memecahkan masalah pengguna dengan HTML, CSS, Javascript dan PHP serta menguasai Framework Laravel yang sudah saya implementasikan ke dalam beberapa project yang sudah saya buat.
                 </p>
                 <Link href='/projects' className='bg-blue text-base text-white font-bold px-4 py-2 rounded'>Contact With Me</Link>
