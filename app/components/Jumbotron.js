7ee8bd487c2a153646908bd7e7b190183569cf33
Rizky Khoiruddin <rizky.khoiruddin17@gmail.com>
2023-12-02 21:39:03 +0700

    02/12/2023

--- app/components/Jumbotron.js
+++ app/components/Jumbotron.js
@@ -4,11 +4,11 @@ import React from 'react'
 
 function Header() {
     return (
-        <div className='flex lg:flex-row flex-col-reverse lg:px-4 px-0 mt-6 lg:gap-0 gap-6'>
+        <div className='flex lg:flex-row flex-col-reverse lg:px-4 px-0 mt-6 lg:gap-0 gap-12'>
             <div className='flex flex-col basis-[50%] items-start justify-center lg:ms-10 ms-0 me-0 gap-5'>
                 <h3 className='text-2xl font-semibold uppercase tracking-wide'>Rizky Khoiruddin</h3>
                 <h1 className='text-blue text-5xl font-bold'>Fullstack Developer</h1>
-                <p className='text-lg leading-7'>
+                <p className='lg:text-lg text-base leading-7'>
                     Saya menerapkan desain antarmuka pengguna dan memecahkan masalah pengguna dengan HTML, CSS, Javascript dan PHP serta menguasai Framework Laravel yang sudah saya implementasikan ke dalam beberapa project yang sudah saya buat.
                 </p>
                 <Link href='/projects' className='bg-blue text-base text-white font-bold px-4 py-2 rounded'>Contact With Me</Link>
