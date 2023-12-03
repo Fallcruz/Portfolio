2ac36a4775251a73273c9cb19864fae0b6de7b1f
Rizky Khoiruddin <rizky.khoiruddin17@gmail.com>
2023-12-03 13:18:15 +0700

    03/12/2023

--- app/components/Skills.js
+++ app/components/Skills.js
@@ -4,7 +4,7 @@ function Skills() {
     return (
         <div className='flex flex-col items-center mt-12 gap-4'>
             <h1 className='text-2xl font-medium'>My Skills</h1>
-            <hr className='w-12 border-t-[3px] border-white rounded'/>
+            <hr className='w-10 border-t-[3px] border-white rounded'/>
         </div>
     )
 }
