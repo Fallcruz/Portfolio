082c22311ab01d51592019cbf0ce177f8c229539
Rizky Khoiruddin <rizky.khoiruddin17@gmail.com>
2024-06-13 14:32:12 +0700

    update v2

--- app/components/Experience.js
+++ app/components/Experience.js
@@ -13,14 +13,14 @@ function Experience() {
                 <div className='lg:w-[80%] w-full flex flex-col gap-5'>
                     <div className='flex items-center lg:gap-6 gap-4'>
                         <Image className='w-auto lg:h-16 h-12 rounded-lg' src='/img/sinarmas_land_logo.jpg' width={500} height={500} quality={100} alt='logo-sinarmas-land'/>
-                        <h2 className='lg:text-4xl text-3xl font-bold text-linear-blue'>Sinarmas Land</h2>
+                        <h2 className='lg:text-4xl text-2xl font-bold text-linear-blue'>Sinarmas Land</h2>
                     </div>
                     <div className='flex lg:flex-row flex-col lg:items-center lg:justify-between lg:gap-0 gap-2'>
                         <div className='flex items-center'>
-                            <h2 className='lg:text-2xl text-xl font-semibold'>OutSystems Developer</h2>
+                            <h2 className='lg:text-2xl text-lg font-semibold'>OutSystems Developer</h2>
                         </div>
                         <div>
-                            <h3 className='lg:text-xl text-lg font-semibold'>June 2024 - Present, Internship, Hybrid</h3>
+                            <h3 className='lg:text-xl text-base font-semibold'>June 2024 - Present, Internship, Hybrid</h3>
                         </div>
                     </div>
                     <div className=''>
@@ -30,7 +30,7 @@ function Experience() {
                     </div>
                     <div className='flex items-center flex-wrap gap-3'>
                         <div className='px-4 py-2 border-linear-blue rounded-[2rem]'>
-                            <h3 className='text-base font-semibold'>OutSystems</h3>
+                            <h3 className='lg:text-base text-sm font-semibold'>OutSystems</h3>
                         </div>
                         {/* <div className='px-4 py-2 border-linear-blue rounded-[2rem]'>
                             <h3 className='text-base font-semibold'>PHP</h3>
@@ -49,18 +49,18 @@ function Experience() {
                 <div className='lg:w-[80%] w-full flex flex-col gap-5'>
                     <div className='flex items-center lg:gap-6 gap-4'>
                         <Image className='w-auto lg:h-16 h-12 rounded-lg' src='/img/all_in_eduspace_logo.jpg' width={500} height={500} quality={100} alt='logo-allin-eduspace'/>
-                        <h2 className='lg:text-4xl text-3xl font-bold text-linear-blue'>ALL-in Eduspace</h2>
+                        <h2 className='lg:text-4xl text-2xl font-bold text-linear-blue'>ALL-in Eduspace</h2>
                     </div>
                     <div className='flex lg:flex-row flex-col lg:items-center lg:justify-between lg:gap-0 gap-2'>
                         <div className='flex items-center'>
-                            <h2 className='lg:text-2xl text-xl font-semibold'>Fullstack Web Developer</h2>
+                            <h2 className='lg:text-2xl text-lg font-semibold'>Fullstack Web Developer</h2>
                         </div>
                         <div>
-                            <h3 className='lg:text-xl text-lg font-semibold'>July 2022 - January 2024, Intership, Hybrid</h3>
+                            <h3 className='lg:text-xl text-base font-semibold'>July 2022 - January 2024, Intership, Hybrid</h3>
                         </div>
                     </div>
                     <div className=''>
-                        <ol className='list-disc list-inside lg:text-lg text-base flex flex-col gap-2'>
+                        <ol className='list-disc list-inside lg:text-lg text-sm flex flex-col gap-2'>
                             <li>Create UI/UX Design for several Landing Pages, Websites and Web Applications</li>
                             <li>Building a database using MySQL</li>
                             <li>Building Websites and Web Applications using the Laravel framework</li>
@@ -69,19 +69,19 @@ function Experience() {
                     </div>
                     <div className='flex items-center flex-wrap gap-3'>
                         <div className='px-4 py-2 border-linear-blue rounded-[2rem]'>
-                            <h3 className='text-base font-semibold'>Laravel</h3>
+                            <h3 className='lg:text-base text-sm font-semibold'>Laravel</h3>
                         </div>
                         <div className='px-4 py-2 border-linear-blue rounded-[2rem]'>
-                            <h3 className='text-base font-semibold'>PHP</h3>
+                            <h3 className='lg:text-base text-sm font-semibold'>PHP</h3>
                         </div>
                         <div className='px-4 py-2 border-linear-blue rounded-[2rem]'>
-                            <h3 className='text-base font-semibold'>JavaScript</h3>
+                            <h3 className='lg:text-base text-sm font-semibold'>JavaScript</h3>
                         </div>
                         <div className='px-4 py-2 border-linear-blue rounded-[2rem]'>
-                            <h3 className='text-base font-semibold'>SQL</h3>
+                            <h3 className='lg:text-base text-sm font-semibold'>SQL</h3>
                         </div>
                         <div className='px-4 py-2 border-linear-blue rounded-[2rem]'>
-                            <h3 className='text-base font-semibold'>Bootstrap</h3>
+                            <h3 className='lg:text-base text-sm font-semibold'>Bootstrap</h3>
                         </div>
                     </div>
                 </div>
