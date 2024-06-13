6f93109d2b3fa674cbddbc9def429b2915482324
Rizky Khoiruddin <rizky.khoiruddin17@gmail.com>
2024-06-13 14:53:48 +0700

    update v2

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -15,12 +15,12 @@ function Navbar() {
     }, [pathname]);
 
     return (
-        <nav className='flex flex-row items-center justify-between fixed top-0 left-0 right-0 lg:py-3 py-2 lg:px-14 px-6 bg-black-secondary z-50 max-w-[1600px] mx-auto'>
+        <nav className='flex flex-row items-center justify-between fixed top-0 left-0 right-0 lg:py-3 py-2 lg:px-14 px-6 bg-black-secondary/60 backdrop-blur-lg z-50 max-w-[1600px] mx-auto'>
             <Link className='flex flex-row items-center gap-1' href='/'>
                 <Image className='lg:w-[72px] lg:h-[72px] w-14 h-14' src='/img/round-profile.png' width={500} height={500} quality={100} alt='logo' priority={true} />
                 <h1 className='mt-1 lg:text-[3.5rem] text-[2.2rem] font-rakkas leading-none lg:tracking-[-0.5rem] tracking-[-0.3rem]'>RK</h1>
             </Link>
-            <ul className={`navbar-menu ${showNavbar ? 'flex' : 'hidden'} lg:flex flex-row lg:w-fit w-[98%] bg-black-secondary items-center lg:justify-end justify-center lg:pt-0 lg:pb-0 pt-4 pb-6 lg:gap-8 gap-2`}>
+            <ul className={`navbar-menu ${showNavbar ? 'flex' : 'hidden'} lg:flex flex-row lg:w-fit w-[98%] lg:bg-[#ffffff00] bg-black-secondary items-center lg:justify-end justify-center lg:pt-0 lg:pb-0 pt-4 pb-6 lg:gap-8 gap-2`}>
                 <li className={`w-fit ${pathname === '/' ? 'active' : ''}`}>
                     <Link href='/' className='font-plus-jakarta-sans lg:text-base text-sm font-medium hover:text-gray'>Home</Link>
                 </li>
@@ -30,9 +30,9 @@ function Navbar() {
                 <li className={`w-fit ${pathname === '/about' ? 'active' : ''}`}>
                     <Link href='/about' className='font-plus-jakarta-sans lg:text-base text-sm font-medium hover:text-gray'>About</Link>
                 </li>
-                <li className='w-fit'>
+                {/* <li className='w-fit'>
                     <Link href='' className='font-plus-jakarta-sans lg:text-base text-sm font-medium hover:text-gray pointer-events-none'>Contacts</Link>
-                </li>
+                </li> */}
             </ul>
             <div className='flex lg:hidden items-center justify-center'>
                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-deep cursor-pointer" width={32} height={32} viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" onClick={() => handleNavbar()}>
