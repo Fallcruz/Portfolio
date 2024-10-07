fd490ba7fd4cec264b1d63b3ba3484ffebaffb90
Rizky Khoiruddin <rizky.khoiruddin17@gmail.com>
2024-10-07 17:14:00 +0700

    update v3

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -10,9 +10,11 @@ export default function Page() {
             <div className='flex lg:flex-row flex-col-reverse lg:px-0 px-0 lg:ms-6 ms-0 mt-6 lg:gap-0 gap-12'>
                 <div className='flex flex-col basis-[50%] items-start justify-center lg:ms-10 ms-0 me-0 gap-5'>
                     <h1 className='text-2xl font-bold uppercase tracking-normal'>Rizky Khoiruddin</h1>
-                    <h2 className='linear-blue text-5xl font-bold tracking-tight pb-1'>Fullstack Developer</h2>
+                    <h2 className='linear-blue text-5xl font-bold tracking-tight pb-1'>Full Stack Developer</h2>
                     <p className='lg:text-base text-sm lg:leading-[30px] leading-6'>
-                        Saya menerapkan desain antarmuka pengguna dan memecahkan masalah pengguna dengan HTML, CSS, Javascript dan PHP serta menguasai Framework Laravel yang sudah saya implementasikan ke dalam beberapa project yang sudah saya buat.
+                        {/* Saya menerapkan desain antarmuka pengguna dan memecahkan masalah pengguna dengan HTML, CSS, Javascript dan PHP serta menguasai Framework Laravel yang sudah saya implementasikan ke dalam beberapa project yang sudah saya buat. */}
+                        Saya adalah seorang Full Stack Developer dengan keahlian dalam membangun aplikasi web yang efisien, responsif, dan mudah digunakan. Berpengalaman dalam pengembangan front-end dan back-end, saya menguasai berbagai bahasa pemrograman dan framework seperti JavaScript (React, Node.js), PHP, Laravel, NextJS serta database seperti PostgreSQL dan MySQL. 
+                        {/* Saya terbiasa bekerja dalam tim kolaboratif dan selalu berfokus pada penulisan kode berkualitas tinggi. Dengan kemampuan menghubungkan desain dengan fungsi, saya memastikan pengalaman pengguna yang mulus di berbagai platform. Baik itu membangun antarmuka pengguna yang dinamis atau merancang logika server-side yang kompleks, saya menghadirkan pendekatan menyeluruh dalam pengembangan web */}
                     </p>
                     <Link href='/projects' className='bg-blue text-base text-white font-bold px-5 lg:py-[10px] py-2 rounded-lg tracking-tight'>My Projects</Link>
                 </div>
--- app/components/Experience.js
+++ app/components/Experience.js
@@ -29,10 +29,10 @@ function Experience() {
                     </div> */}
                     <div className='flex items-center flex-wrap gap-3'>
                         <div className='px-4 py-2 border-linear-blue rounded-[2rem]'>
-                            <h3 className='lg:text-base text-xs font-semibold'>Jira</h3>
+                            <h3 className='lg:text-sm text-xs font-semibold'>Jira</h3>
                         </div>
                         <div className='px-4 py-2 border-linear-blue rounded-[2rem]'>
-                            <h3 className='lg:text-base text-xs font-semibold'>OutSystems</h3>
+                            <h3 className='lg:text-sm text-xs font-semibold'>OutSystems</h3>
                         </div>
                     </div>
                 </div>
@@ -43,14 +43,14 @@ function Experience() {
                     </div>
                     <div className='flex lg:flex-row flex-col lg:items-center lg:justify-between lg:gap-0 gap-1'>
                         <div className='flex items-center'>
-                            <h2 className='lg:text-xl text-lg font-semibold'>Fullstack Web Developer</h2>
+                            <h2 className='lg:text-xl text-lg font-semibold'>Full Stack Web Developer</h2>
                         </div>
                         <div>
                             <h3 className='lg:text-lg text-base font-semibold'>July 2022 - January 2024, Intership, Hybrid</h3>
                         </div>
                     </div>
                     <div className='ms-4'>
-                        <ol className='list-disc list-outside lg:text-lg text-sm flex flex-col gap-2'>
+                        <ol className='list-disc list-outside lg:text-base text-sm flex flex-col gap-2'>
                             <li>Create UI/UX Design for several Landing Pages, Websites and Web Applications</li>
                             <li>Building a database using MySQL</li>
                             <li>Building Websites and Web Applications using the Laravel framework</li>
@@ -59,19 +59,19 @@ function Experience() {
                     </div>
                     <div className='flex items-center flex-wrap gap-3'>
                         <div className='px-4 py-2 border-linear-blue rounded-[2rem]'>
-                            <h3 className='lg:text-base text-xs font-semibold'>Laravel</h3>
+                            <h3 className='lg:text-sm text-xs font-semibold'>Laravel</h3>
                         </div>
                         <div className='px-4 py-2 border-linear-blue rounded-[2rem]'>
-                            <h3 className='lg:text-base text-xs font-semibold'>PHP</h3>
+                            <h3 className='lg:text-sm text-xs font-semibold'>PHP</h3>
                         </div>
                         <div className='px-4 py-2 border-linear-blue rounded-[2rem]'>
-                            <h3 className='lg:text-base text-xs font-semibold'>JavaScript</h3>
+                            <h3 className='lg:text-sm text-xs font-semibold'>JavaScript</h3>
                         </div>
                         <div className='px-4 py-2 border-linear-blue rounded-[2rem]'>
-                            <h3 className='lg:text-base text-xs font-semibold'>SQL</h3>
+                            <h3 className='lg:text-sm text-xs font-semibold'>SQL</h3>
                         </div>
                         <div className='px-4 py-2 border-linear-blue rounded-[2rem]'>
-                            <h3 className='lg:text-base text-xs font-semibold'>Bootstrap</h3>
+                            <h3 className='lg:text-sm text-xs font-semibold'>Bootstrap</h3>
                         </div>
                     </div>
                 </div>
