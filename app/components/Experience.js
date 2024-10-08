d1396e8e5e98afe4d6a6efde06909096cf3fcf12
Rizky Khoiruddin <rizky.khoiruddin17@gmail.com>
2024-10-08 09:26:27 +0700

    update v3

--- app/components/Experience.js
+++ app/components/Experience.js
@@ -19,14 +19,16 @@ function Experience() {
                             <h2 className='lg:text-xl text-lg font-semibold'>Application Development</h2>
                         </div>
                         <div>
-                            <h3 className='lg:text-lg text-base font-semibold'>June 2024 - September 2024, Internship, Hybrid</h3>
+                            <h3 className='lg:text-lg text-base font-semibold'>June 2024 - September 2024, Internship, Remote</h3>
                         </div>
                     </div>
-                    {/* <div className=''>
-                        <ol className='list-disc list-outside lg:text-lg text-base flex flex-col gap-2'>
-                            <li>lorem ipsum</li>
+                    <div className='ms-4'>
+                        <ol className='list-disc list-outside lg:text-base text-sm flex flex-col gap-2'>
+                            <li>Mengelola beberapa Website dan Aplikasi menggunakan platform Outsystems</li>
+                            <li>Meningkatkan performa dan efisiensi resource pada beberapa Website dan Aplikasi</li>
+                            <li>Mengelola resource yang sudah tidak digunakan pada beberapa Website dan Aplikasi</li>
                         </ol>
-                    </div> */}
+                    </div>
                     <div className='flex items-center flex-wrap gap-3'>
                         <div className='px-4 py-2 border-linear-blue rounded-[2rem]'>
                             <h3 className='lg:text-sm text-xs font-semibold'>Jira</h3>
@@ -46,15 +48,15 @@ function Experience() {
                             <h2 className='lg:text-xl text-lg font-semibold'>Full Stack Web Developer</h2>
                         </div>
                         <div>
-                            <h3 className='lg:text-lg text-base font-semibold'>July 2022 - January 2024, Intership, Hybrid</h3>
+                            <h3 className='lg:text-lg text-base font-semibold'>July 2022 - January 2024, Internship, Hybrid</h3>
                         </div>
                     </div>
                     <div className='ms-4'>
                         <ol className='list-disc list-outside lg:text-base text-sm flex flex-col gap-2'>
-                            <li>Create UI/UX Design for several Landing Pages, Websites and Web Applications</li>
-                            <li>Building a database using MySQL</li>
-                            <li>Building Websites and Web Applications using the Laravel framework</li>
-                            <li>Updating existing Websites and Web Applications</li>
+                            <li>Membuat Desain UI/UX untuk beberapa Landing Page, Website, dan Web Application</li>
+                            <li>Membangun database menggunakan MySQL</li>
+                            <li>Membangun Website dan Web Application menggunakan framework Laravel</li>
+                            <li>UMemperbarui Website dan Web Application yang sudah ada</li>
                         </ol>
                     </div>
                     <div className='flex items-center flex-wrap gap-3'>
